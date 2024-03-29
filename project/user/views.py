01a5226026a7573d25f8fd3f2070cb454e9ffe19
ashwini <ashwinib2001nov@gmail.com>
2024-03-29 22:12:47 +0530

    Your commit message here

--- project/user/views.py
+++ project/user/views.py
@@ -1,46 +1,5 @@
-from django.shortcuts import render
-
-#################### index#######################################
-def index(request):
-    return render(request, 'user/index.html', {'title':'index'})
-
-########### register here #####################################
-def register(request):
-    if request.method == 'POST':
-       form = UserRegisterForm(request.POST)
-       if form.is_valid():
-          form.save()
-          username = form.cleaned_data.get('username')
-          email = form.cleaned_data.get('email')
-          ######################### mail system ####################################
-          htmly = get_template('user/Email.html')
-          d = { 'username': username }
-          subject, from_email, to = 'welcome', 'anithamurai@gmail.com', email
-          html_content = htmly.render(d)
-          msg = EmailMultiAlternatives(subject, html_content, from_email, [to])
-          msg.attach_alternative(html_content, "text/html")
-          msg.send()
-          ##################################################################
-          messages.success(request, f'Your account has been created ! You are now able to log in')
-          return redirect('login')
-    else:
-       form = UserRegisterForm()
-    return render(request, 'user/register.html', {'form': form, 'title':'register here'})
-
-################ login forms###################################################
-def Login(request):
-    if request.method == 'POST':
-
-       # AuthenticationForm_can_also_be_used__
-
-       username = request.POST['username']
-       password = request.POST['password']
-       user = authenticate(request, username = username, password = password)
-       if user is not None:
-          form = login(request, user)
-          messages.success(request, f' welcome {username} !!')
-          return redirect('index')
-       else:
-          messages.info(request, f'account done not exit plz sign in')
-    form = AuthenticationForm()
-    return render(request, 'user/login.html', {'form':form, 'title':'log in'})
+from django.views.decorators.http
+import require_http_methods
+@require_http_methods(["GET","POST"])
+def my_view(request):
+    pass
